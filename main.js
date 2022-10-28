5609377bce4c581554257b51b81594f6ba562aa8
MarcoPerta <perta.marco@gmail.com>
2022-10-28 16:18:31 +0200

    fatto fino a lezione stamattina

--- main.js
+++ main.js
@@ -7,3 +7,32 @@
 // - con difficoltà 1 => 100 caselle, con un numero compreso tra 1 e 100, divise in 10 caselle per 10 righe;
 // - con difficoltà 2 => 81 caselle, con un numero compreso tra 1 e 81, divise in 9 caselle per 9 righe;
 // - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe;
+
+// -importo il div id griglia nel JS
+// -creazione div quadrato con funzione che andrà dentro griglia
+// -stilizzazione CSS quadrato, flexato padre contenitore
+// -ciclo for per creare tot quadrati e click col this
+
+const griglia = document.getElementById('griglia');
+console.log(griglia);
+
+function creazioneQuadrato(){
+    const div = document.createElement('div');
+    div.classList.add('quadrato');
+    return div;
+}
+console.log(creazioneQuadrato())
+
+for (let i=0; i<64; i++){
+
+    let elementoCorrente = creazioneQuadrato();
+    console.log = (elementoCorrente);
+
+    elementoCorrente.addEventListener('click', function(){
+        console.log(this)
+        this.classList.toggle('attiva');
+    })
+
+
+    griglia.append(creazioneQuadrato())
+}
